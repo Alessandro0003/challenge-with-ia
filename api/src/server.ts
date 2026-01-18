4875a3a61a8132754e4a49f7e88ec1a1aa203e29
Alessandro0003 <alessandroquinta3@gmail.com>
2026-01-18 18:08:50 -0300

    :wrench: chore: configurando servidor e documentation

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,7 +1,40 @@
 import { fastify } from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  jsonSchemaTransform,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifyCors } from '@fastify/cors'
+import ScalarApiReferences from '@scalar/fastify-api-reference'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifyCors, {
+  origin: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS']
+})
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Challenge With IA',
+      description: 'API for capturigin and inspecting webhook requests',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(ScalarApiReferences, {
+  routePrefix: '/docs'
+})
 
 app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
-  console.log('🚀 Server is running!')
+  console.log('🚀 HTPP server running on http://localhost:3333')
+  console.log('📚 Docs available at http://localhost:3333/docs')
 })
